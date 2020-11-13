a399f6b8292b897df29ead80ca157e6f94e4df75
Alison Williams <apw5011@gmail.com>
2020-11-13 20:03:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,10 @@
 def key_for_min_value(name_hash)
   small_key = ""
   name_hash.each do |name_key, name_value|
-    
+    distance_from_zero = 0 - name_value
+    if name_value <= distance_from_zero 
+      small_key = name_key
+    end
   end
 small_key
 end
